53be307909e37c1d4ca39fd2621c24c49a3d78e5
vikasgoudarRG <vikasgoudar123@gmail.com>
2025-05-27 09:24:47 +0530

    Update climbing_stairs.cpp

--- dynamic_programming/climbing_stairs.cpp
+++ dynamic_programming/climbing_stairs.cpp
@@ -3,6 +3,9 @@
 class Solution {
 public:
     int climbStairs(int n) {
+        
+            // dynamic programming approach
+
         int step_a = 1;
         int step_b = 1;
 
@@ -13,5 +16,25 @@ public:
         }
 
         return step_b;
+        
+
+        /*
+            // recursion with memoization
+
+        vector<int> memo(n+1, 0);
+        return recur(0, n, memo);
+        */
+    }
+    /*
+    int recur(int i, int n, vector<int>& memo) {
+        if (i > n) return 0;
+        if (i == n) return 1;
+
+        if (memo[i] > 0) {
+            return memo[i];
+        }
+        memo[i] = recur(i+1, n, memo) + recur(i+2, n, memo);
+        return memo[i];
+        */
     }
 };
