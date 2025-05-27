3de87229ba90c19e0e3a1faada9535ad1598250c
vikasgoudarRG <vikasgoudar123@gmail.com>
2025-05-27 10:25:57 +0530

    Update climbing_stairs.cpp

--- dynamic_programming/climbing_stairs.cpp
+++ dynamic_programming/climbing_stairs.cpp
@@ -3,7 +3,15 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        
+
+            // matrix fibonacci method
+        if (n == 0) return 1;
+        vector<vector<long long>> A = { { 1, 1 }, { 1, 0 } };
+        vector<vector<long long>> res = bin_pow(A, n - 1);
+        return res[0][0] + res[0][1];
+
+
+        /*
             // dynamic programming approach
 
         int step_a = 1;
@@ -16,7 +24,7 @@ public:
         }
 
         return step_b;
-        
+        */
 
         /*
             // recursion with memoization
@@ -25,6 +33,7 @@ public:
         return recur(0, n, memo);
         */
     }
+
     /*
     int recur(int i, int n, vector<int>& memo) {
         if (i > n) return 0;
@@ -35,6 +44,31 @@ public:
         }
         memo[i] = recur(i+1, n, memo) + recur(i+2, n, memo);
         return memo[i];
-        */
+        
+    }
+    */
+
+    vector<vector<long long>> bin_pow(vector<vector<long long>> a, int n) {
+        vector<vector<long long>> res = { { 1, 0 }, { 0, 1 }};
+        while (n > 0) {
+            if (n & 1) {
+                res = mult(res, a);
+            }
+            a = mult(a, a);
+            n >>= 1;
+        }
+        return res;
+    }
+
+    vector<vector<long long>> mult(const vector<vector<long long>>& a, const vector<vector<long long>>& b) {
+        vector<vector<long long>> res(a.size(), vector<long long>(b[0].size(), 0));
+        for (int i = 0; i < a.size(); ++i) {
+            for (int j = 0; j < b[0].size(); ++j) {
+                for (int k = 0; k < a[0].size(); ++k) {
+                    res[i][j] += a[i][k] * b[k][j];
+                }
+            }
+        }
+        return res;
     }
 };
