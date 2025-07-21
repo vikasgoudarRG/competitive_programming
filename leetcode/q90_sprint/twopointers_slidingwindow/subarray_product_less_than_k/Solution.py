61f3232cab47335a513b080b847502cb888d6daa
vikasgoudarRG <vikasgoudar123@gmail.com>
2025-07-21 23:28:54 +0530

    add soln - subarray product less than k - py

--- leetcode/q90_sprint/twopointers_slidingwindow/subarray_product_less_than_k/Solution.py
+++ leetcode/q90_sprint/twopointers_slidingwindow/subarray_product_less_than_k/Solution.py
@@ -1,8 +1,11 @@
 from typing import List
+import math
 
 class Solution:
     def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
-        return self.my_solution(nums, k)
+        # my solution is approach 1 only
+        # return self.my_solution(nums, k)
+        return self.prefix_log(nums, k)
 
 
     # Sliding Window
@@ -32,5 +35,40 @@ class Solution:
             high += 1
         
         return ans
+
+    # Prefix Sum
+    # Time Complexity O(n * logn)
+    # Space Complexity O(n)
+    # Usefull when cases exists where product of subarrays cause overflow
+    # thus we convert problem from products to sum of logs
+    # a*b*c = k becomes log(a) + log(b) + log(c) = log(k)
+    # a setback is that log gives float, whose operations aren't always accurate and need buffer
+    def prefix_log(self, nums: List[int], k: int) -> int:
+        if (k < 1):
+            return 0
+
+        n: int = len(nums)
+        log_k: float = math.log(k)
+        prefix_sum = 0
+
+        low: int = 0
+        high: int = 0
+        ans: int = 0
+
+        while high < n:
+            prefix_sum += math.log(nums[high])
+
+            while (low <= high and prefix_sum >= log_k - 1e-9):
+                prefix_sum -= math.log(nums[low])
+                low += 1
+            
+            ans += (high - low + 1)
+            high += 1
+        
+        return ans
+            
+
+
+
 
 
